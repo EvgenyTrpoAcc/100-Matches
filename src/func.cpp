9e8afa5041c6770923caf569cd5ad3a80721f577
Raddewolf <mrslimegood@gmail.com>
2018-05-14 17:24:44 +0700

    Add func.cpp and func.h

---
+++ src/func.cpp
@@ -0,0 +1,6 @@
+#pragma warning (disable : 4996)
+
+#include <iostream>
+
+#include "func.h"
+
---
+++ src/func.h
@@ -0,0 +1,12 @@
+#ifndef _FUNC_H_
+#define _FUNC_H_
+
+using namespace std;
+
+struct gamer
+{
+	bool status;
+	string name;
+};
+
+#endif

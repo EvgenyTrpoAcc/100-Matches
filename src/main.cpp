93e7ed3d671544f591a469232d4d379f844d60b1
UsagiLoliChan <miracle121790@gmail.com>
2018-05-25 18:41:18 +0700

    Fix output on the window

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,16 @@ int main()
 {
 	RenderWindow window(VideoMode(800, 450), "100 matches");
 	string str;
-	char ch;
 
 	Font font;
 	font.loadFromFile("Things/11776.otf");
 
 	Text text("", font, 35), mod("", font, 50);
 
-	text.setColor(Color::Color(0, 230, 0, 230));
+	text.setFillColor(Color(0, 230, 0, 230));
 	text.setStyle(Text::Bold);
 
-	mod.setColor(Color::Cyan);
+	mod.setFillColor(Color::Cyan);
 	mod.setStyle(Text::Bold);
 
 	Image sp;
